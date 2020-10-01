adfeb1f447aeef18f500904a7b8586aff9b07fa6
Hien Nguyen <hiennguyen@appvity.com>
2020-10-01 12:04:02 +0700

    222

--- ASPNETCoreWebApplication/Startup.cs
+++ ASPNETCoreWebApplication/Startup.cs
@@ -27,7 +27,7 @@ namespace ASPNETCoreWebApplication
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("We are BIS team (master branch) 111 !!!");
+                await context.Response.WriteAsync("We are BIS team (master branch) 111 222 !!!");
             });
         }
     }

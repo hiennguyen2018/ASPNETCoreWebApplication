15206d980e78bc31f2ab8879243b6d3a41484ff6
Hien Nguyen <hiennguyen@appvity.com>
2020-10-01 13:19:57 +0700

    Checkin master again

--- ASPNETCoreWebApplication/Startup.cs
+++ ASPNETCoreWebApplication/Startup.cs
@@ -27,7 +27,7 @@ namespace ASPNETCoreWebApplication
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("We are BIS team!!!");
+                await context.Response.WriteAsync("We are BIS team. Tks!!!");
             });
         }
     }
